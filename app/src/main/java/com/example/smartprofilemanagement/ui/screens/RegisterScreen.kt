4376bee02fb079b7dd49320fb8c7a647df5280f4
Arishkhan12 <hamzakhalidmepco@gmail.com>
2023-10-18 01:13:58 +0500

    newproject

---
+++ app/src/main/java/com/example/smartprofilemanagement/ui/screens/RegisterScreen.kt
@@ -0,0 +1,10 @@
+package com.example.smartprofilemanagement.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable 
+fun ViewScreen(navController: NavController) {
+    Text(text = "View Screen")
+}
